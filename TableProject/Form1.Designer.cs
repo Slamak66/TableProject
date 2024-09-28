c4a2b79a51b8d1882813ee02d224cbec0799c941
Slamak66 <79204343+Slamak66@users.noreply.github.com>
2024-09-28 20:36:12 +0200

    Add project files.

---
+++ TableProject/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace TableProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbbModel = new System.Windows.Forms.ComboBox();
+            this.lblModelPick = new System.Windows.Forms.Label();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.lblDateTo = new System.Windows.Forms.Label();
+            this.lblDateFrom = new System.Windows.Forms.Label();
+            this.dgvOutput = new System.Windows.Forms.DataGridView();
+            this.NamePriceNoVat = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.priceVat = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMain
+            // 
+            this.dgvMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMain.Location = new System.Drawing.Point(12, 46);
+            this.dgvMain.Name = "dgvMain";
+            this.dgvMain.RowHeadersWidth = 51;
+            this.dgvMain.RowTemplate.Height = 29;
+            this.dgvMain.Size = new System.Drawing.Size(576, 419);
+            this.dgvMain.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 11);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(163, 29);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Načíst ze souboru";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(329, 84);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(96, 29);
+            this.btnPrint.TabIndex = 2;
+            this.btnPrint.Text = "Vypsat cenu";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.cbbModel);
+            this.panel1.Controls.Add(this.lblModelPick);
+            this.panel1.Controls.Add(this.dtpTo);
+            this.panel1.Controls.Add(this.dtpFrom);
+            this.panel1.Controls.Add(this.lblDateTo);
+            this.panel1.Controls.Add(this.lblDateFrom);
+            this.panel1.Controls.Add(this.btnPrint);
+            this.panel1.Location = new System.Drawing.Point(159, 469);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(429, 116);
+            this.panel1.TabIndex = 3;
+            // 
+            // cbbModel
+            // 
+            this.cbbModel.FormattingEnabled = true;
+            this.cbbModel.Location = new System.Drawing.Point(64, 5);
+            this.cbbModel.Name = "cbbModel";
+            this.cbbModel.Size = new System.Drawing.Size(138, 28);
+            this.cbbModel.TabIndex = 8;
+            // 
+            // lblModelPick
+            // 
+            this.lblModelPick.AutoSize = true;
+            this.lblModelPick.Location = new System.Drawing.Point(3, 10);
+            this.lblModelPick.Name = "lblModelPick";
+            this.lblModelPick.Size = new System.Drawing.Size(55, 20);
+            this.lblModelPick.TabIndex = 7;
+            this.lblModelPick.Text = "Model:";
+            // 
+            // dtpTo
+            // 
+            this.dtpTo.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpTo.Location = new System.Drawing.Point(300, 38);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.Size = new System.Drawing.Size(125, 27);
+            this.dtpTo.TabIndex = 6;
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFrom.Location = new System.Drawing.Point(300, 5);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(125, 27);
+            this.dtpFrom.TabIndex = 5;
+            // 
+            // lblDateTo
+            // 
+            this.lblDateTo.AutoSize = true;
+            this.lblDateTo.Location = new System.Drawing.Point(262, 43);
+            this.lblDateTo.Name = "lblDateTo";
+            this.lblDateTo.Size = new System.Drawing.Size(32, 20);
+            this.lblDateTo.TabIndex = 4;
+            this.lblDateTo.Text = "Do:";
+            // 
+            // lblDateFrom
+            // 
+            this.lblDateFrom.AutoSize = true;
+            this.lblDateFrom.Location = new System.Drawing.Point(262, 10);
+            this.lblDateFrom.Name = "lblDateFrom";
+            this.lblDateFrom.Size = new System.Drawing.Size(32, 20);
+            this.lblDateFrom.TabIndex = 3;
+            this.lblDateFrom.Text = "Od:";
+            // 
+            // dgvOutput
+            // 
+            this.dgvOutput.AllowUserToAddRows = false;
+            this.dgvOutput.AllowUserToDeleteRows = false;
+            this.dgvOutput.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvOutput.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.ActiveBorder;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvOutput.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOutput.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NamePriceNoVat,
+            this.priceVat});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvOutput.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvOutput.EnableHeadersVisualStyles = false;
+            this.dgvOutput.Location = new System.Drawing.Point(659, 46);
+            this.dgvOutput.Name = "dgvOutput";
+            this.dgvOutput.ReadOnly = true;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.ActiveBorder;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvOutput.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvOutput.RowHeadersVisible = false;
+            this.dgvOutput.RowHeadersWidth = 51;
+            this.dgvOutput.RowTemplate.Height = 45;
+            this.dgvOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dgvOutput.Size = new System.Drawing.Size(280, 419);
+            this.dgvOutput.TabIndex = 4;
+            // 
+            // NamePriceNoVat
+            // 
+            this.NamePriceNoVat.HeaderText = "Název modelu   Cena bez DPH";
+            this.NamePriceNoVat.MinimumWidth = 6;
+            this.NamePriceNoVat.Name = "NamePriceNoVat";
+            this.NamePriceNoVat.ReadOnly = true;
+            this.NamePriceNoVat.Width = 150;
+            // 
+            // priceVat
+            // 
+            this.priceVat.HeaderText = "                     Cena s DPH";
+            this.priceVat.MinimumWidth = 6;
+            this.priceVat.Name = "priceVat";
+            this.priceVat.ReadOnly = true;
+            this.priceVat.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 592);
+            this.Controls.Add(this.dgvOutput);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dgvMain);
+            this.Name = "Form1";
+            this.Text = "TablePSW";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOutput)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dgvMain;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblDateFrom;
+        private System.Windows.Forms.Label lblModelPick;
+        private System.Windows.Forms.DateTimePicker dtpTo;
+        private System.Windows.Forms.DateTimePicker dtpFrom;
+        private System.Windows.Forms.Label lblDateTo;
+        private System.Windows.Forms.ComboBox cbbModel;
+        private System.Windows.Forms.DataGridView dgvOutput;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NamePriceNoVat;
+        private System.Windows.Forms.DataGridViewTextBoxColumn priceVat;
+    }
+}
+
---
+++ TableProject/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace TableProject
+{
+    public partial class Form1 : Form
+    {
+        DataTable ActiveTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "XML Files|*.xml|All Files|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Title = "Select a File";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                XDocument xDoc = XDocument.Load(filePath);
+
+                ActiveTable = new DataTable();
+
+                var veh1 = xDoc.Descendants("vehicle").FirstOrDefault();
+                if(veh1 != null)
+                {
+                    foreach (var element in veh1.Elements())
+                    {
+                        string ColumnName = element.Name.LocalName;
+                        string DataType = element.Attribute("type")?.Value;
+                        DataColumn activeColumn = new DataColumn();
+                        activeColumn.ColumnName = ColumnName;
+                        string ActiveType = "System." + DataType;
+                        activeColumn.DataType = System.Type.GetType(ActiveType);
+                        activeColumn.AllowDBNull = false;
+                        activeColumn.Caption = ColumnName;
+                        ActiveTable.Columns.Add(activeColumn);
+                    }
+                }
+
+                foreach (var vehicle in xDoc.Descendants("vehicle"))
+                {
+                    DataRow activeRow = ActiveTable.NewRow();
+
+                    foreach(var element in vehicle.Elements())
+                    {
+                        string ColumnName = element.Name.LocalName;
+
+                        activeRow[ColumnName] = element.Value;
+                    }
+                    ActiveTable.Rows.Add(activeRow);
+                }
+
+                dgvMain.DataSource = ActiveTable;
+
+                foreach (DataGridViewColumn column in dgvMain.Columns)
+                {
+                    if (column.Name == "Cena")
+                    {
+                        column.DefaultCellStyle.Format = "C";
+                    }
+                }
+
+                List<string> modelNames = ActiveTable.AsEnumerable()
+                    .Select(row => row.Field<string>("NázevModelu"))
+                    .Distinct()
+                    .ToList();
+                cbbModel.Items.Clear();
+                cbbModel.Items.AddRange(modelNames.ToArray());
+                cbbModel.Items.Add("All");
+            }
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            if(cbbModel.SelectedItem == null)
+            {
+                return;
+            }
+            string selectedModel = cbbModel.SelectedItem.ToString();
+            DateTime fromDate = dtpFrom.Value.Date;
+            DateTime toDate = dtpTo.Value.Date;
+
+            List<string> modelNamesList = new List<string>();
+            List<double> totalPricesList = new List<double>();
+            List<double> totalPricesWithVATList = new List<double>();
+
+            List<string> formattedResultsList = new List<string>();
+            List<string> formattedVATResultsList = new List<string>();
+
+            foreach (DataGridViewRow row in dgvMain.Rows)
+            {
+                if (row.Cells["NázevModelu"].Value == null || row.Cells["DatumProdeje"].Value == null || row.Cells["Cena"].Value == null || row.Cells["DPH"].Value == null)
+                {
+                    continue;
+                }
+                string modelName = row.Cells["NázevModelu"].Value.ToString();
+                DateTime dateOfSale = Convert.ToDateTime(row.Cells["DatumProdeje"].Value);
+                double priceNoVAT = Convert.ToDouble(row.Cells["Cena"].Value);
+                double VAT = Convert.ToDouble(row.Cells["DPH"].Value) / 100;
+                double priceWithVAT = priceNoVAT + (priceNoVAT * VAT);
+
+                if ((selectedModel == "All" || modelName == selectedModel) && (dateOfSale >= fromDate && dateOfSale <= toDate))
+                {
+                    int modelIndex = modelNamesList.IndexOf(modelName);
+                    if (modelIndex == -1)
+                    {
+                        modelNamesList.Add(modelName);
+                        totalPricesList.Add(priceNoVAT);
+                        totalPricesWithVATList.Add(priceWithVAT);
+                    }
+                    else
+                    {
+                        totalPricesList[modelIndex] += priceNoVAT;
+                        totalPricesWithVATList[modelIndex] += priceWithVAT;
+                    }
+                }
+            }
+
+            foreach (var modelName in modelNamesList)
+            {
+                int modelIndex = modelNamesList.IndexOf(modelName);
+                double priceNoVAT = totalPricesList[modelIndex];
+                double priceWithVAT = totalPricesWithVATList[modelIndex];
+
+                string formattedString = $"{modelName}\n{priceNoVAT:C}";
+                string formattedVATString = $"\n{priceWithVAT:C}";
+
+                formattedResultsList.Add(formattedString);
+                formattedVATResultsList.Add(formattedVATString);
+            }
+
+
+            dgvOutput.Rows.Clear();
+
+            for(int i = 0; i < modelNamesList.Count; i++)
+            {
+                int rowIndex = dgvOutput.Rows.Add();
+
+                dgvOutput.Rows[rowIndex].Cells[0].Value = formattedResultsList[i];
+                dgvOutput.Rows[rowIndex].Cells[1].Value = formattedVATResultsList[i];
+            }
+
+            dgvOutput.Refresh();
+        }
+    }
+}
